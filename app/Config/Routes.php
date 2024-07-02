3feb351e3896bfd8be56371d71ed74d7dd99c39e
mhmmddzaky <muh.dzaky1919@gmail.com>
2024-07-02 21:20:32 +0700

    membuat route Fixes #9

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->resource('pendaftaranlomba');
+$routes->resource('pengguna');
+$routes->resource('lomba');
+$routes->resource('kategorilomba');
+$routes->resource('admin');
+$routes->post('updateStatusLomba', 'Lomba:: updateStatusLomba');
